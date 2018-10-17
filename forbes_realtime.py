c26757b47c3cc97f5521f0bc9f27382ebcdc6d48
patelnilesh1810 <patelnilesh1810@gmail.com>
2018-10-17 12:48:01 -0400

    adding other selenium code

---
+++ forbes_realtime.py
@@ -0,0 +1,35 @@
+#import selenium package
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import csv
+import re
+
+driver = webdriver.Chrome(r'C:\Users\patel\OneDrive\Desktop\chromedriver.exe')
+driver.get("https://www.forbes.com/billionaires/list/50/#version:realtime")
+
+csv_file = open('reviews.csv', 'w', encoding='utf-8')
+writer = csv.writer(csv_file)
+
+Rank = list(map(lambda x: x.text, driver.find_elements_by_xpath('//tbody/*[@class="data"]/td[@class = "rank"]')))
+Name =list(map(lambda x: x.text, driver.find_elements_by_xpath('//tbody/*[@class="data"]/td[@class = "name"]')))
+Net_Worth = list(map(lambda x: x.text, driver.find_elements_by_xpath('//tbody/*[@class="data"]/td[@class = "networth"]')))
+wait_button = WebDriverWait(driver, 10)
+
+
+Age = list(map(lambda x: x.text, driver.find_elements_by_xpath('//tbody/*[@class="data"]//td[6]')))
+Source = list(map(lambda x: x.text, driver.find_elements_by_xpath('//tbody/*[@class="data"]//td[7]')))
+Country = list(map(lambda x: x.text, driver.find_elements_by_xpath('//tbody/*[@class="data"]//td[8]')))
+wait_button = WebDriverWait(driver, 10)
+
+Rank = [(re.sub('[\n , . - #]+',"", i)) for i in Rank]
+Name = [(re.sub('[\n,.-]+',"", i)) for i in Name]
+Net_Worth = [(re.sub('[\n , - # B $]+',"", i)) for i in Net_Worth]
+Age = [(re.sub('[\n,.-]', '', i)) for i in Age]
+Source = [(re.sub('[\n,.-]', '', i)) for i in Source]
+Country = [(re.sub('[\n,.-]', '', i)) for i in Country]
+
+data_dict = zip(Rank, Name, Net_Worth, Age, Source, Country)
+
+writer.writerows(data_dict)
+csv_file.close()
+driver.close()
